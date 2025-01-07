dab892dc8b9a395f7d028981cb1478195f1adff3
psmortis <ps.mortis@gmail.com>
2025-01-07 19:45:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ session = cnx.session()
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
-st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True
+st_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
